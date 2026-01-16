1749c369bc7084a2a9d13f24bcb7fdb13a6df736
Ricardo Miller <lazerx38@gmail.com>
2026-01-15 21:26:37 -0500

    Minor Edits

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextType, TextNode
+
+def main():
+    print(TextNode("hello world", "thing"))
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text;
+        self.text_type = text_type;
+        self.url = url;
+
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            if [self.text, self.text_type, self.url] == [other.text, other.text_type, other.url]:
+                return True
+        return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
